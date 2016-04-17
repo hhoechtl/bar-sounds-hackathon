7f03485366fd07083535b0fc4a786b4a9c0d89ec
Hans Höchtl <jhoechtl@gmail.com>
2016-04-17 05:45:25 +0200

    enter track API

--- backend/app.ts
+++ backend/app.ts
@@ -8,6 +8,15 @@ var http = require("http");
 var app = require("koa")();
 var routerUnauthenticated = require("koa-router")();
 var jwt = require('koa-jwt');
+// Init gracenote API
+var Gracenote = require("node-gracenote");
+var gracenoteClientId = "1856440932";
+var gracenoteClientTag = "D9B2C858CF333BE4C924D82209CC3FA8";
+var gracenoteUserId = null;
+var gracenoteApi = new Gracenote(gracenoteClientId, gracenoteClientTag, gracenoteUserId);
+gracenoteApi.register(function (err, uid) {
+    gracenoteUserId = uid;
+});
 
 var jwtCheck = jwt({
     secret: new Buffer('AEj8CLM548f_NfQd_ZBao6Vnl8zKHJpUU7EfT3qha6q8WGhrRnk9sdeY7XTFdUif', 'base64'),
@@ -27,6 +36,16 @@ routerUnauthenticated.get("/tracks/:barId", async function () {
     conn.close();
 });
 
+routerUnauthenticated.get('/leaderBoard', async function () {
+    var conn = await r.connect(AppConfig.dbConfig);
+    this.body = await r.table("users")
+        .filter({ location: this.params.barId })
+        .orderBy({ index: r.desc("tagCounter") })
+        .limit(100)
+        .run(conn);
+    conn.close();
+});
+
 routerUnauthenticated.post("/search/bar", async function () {
     if (this.request.body.latitude && this.request.body.longitude) {
         var currentLocation = r.point(this.request.body.latitude, this.request.body.longitude);
@@ -37,6 +56,14 @@ routerUnauthenticated.post("/search/bar", async function () {
         this.body = [];
     }
 });
+
+/*
+ * TODO: make this work
+routerUnauthenticated.post('/search/track', async function (next){
+    this.body = await gracenoteApi.searchTrack(this.request.body.artist, this.request.body.album, this.request.body.title);
+});
+*/
+
 app.use(routerUnauthenticated.routes());
 
 /**
@@ -45,15 +72,23 @@ app.use(routerUnauthenticated.routes());
 //app.use(jwtCheck);
 
 var routerAuthenticated = require("koa-router")();
-routerAuthenticated.post('/api/track/create', async function () {
-    var conn = await r.connect(AppConfig.dbConfig);
-    this.body = await r.table("messages").insert({
-        user: this.request.body.user,
-        text: this.request.body.text,
-        time: r.now(),
-    }).run(conn);
-    conn.close();
+routerAuthenticated.post('/enter/:barId', async function () {
+    if (this.request.body.title && this.request.body.artist) {
+        var conn = await r.connect(AppConfig.dbConfig);
+        this.body = await r.table("tracks").insert({
+            artist: this.request.body.artist,
+            title: this.request.body.title,
+            location: this.params.barId,
+            album: this.request.body.album ? this.request.body.album : '',
+            begin: r.now(),
+        }).run(conn);
+        conn.close();
+    } else {
+        this.body = {};
+    }
 });
+
+app.use(routerAuthenticated.routes());
 
 /**
  * Initialize server
@@ -75,31 +110,43 @@ var io = sockio(server);
         // Ensure locations table exists
         await r.tableList().contains('locations').do(
             function (tableExists) {
-                return r.branch(tableExists, {created: 0}, r.tableCreate('locations'));
+                return r.branch(tableExists, { created: 0 }, r.tableCreate('locations'));
             }
         ).run(conn);
         // Ensure tracks table exists
         await r.tableList().contains('tracks').do(
             function (tableExists) {
-                return r.branch(tableExists, {created: 0}, r.tableCreate('tracks'));
+                return r.branch(tableExists, { created: 0 }, r.tableCreate('tracks'));
+            }
+        ).run(conn);
+        // Ensure users table exists
+        await r.tableList().contains('users').do(
+            function (tableExists) {
+                return r.branch(tableExists, { created: 0 }, r.tableCreate('users'));
             }
         ).run(conn);
         // Ensure geospatial index exists on locations table field location
         await r.table('locations').indexList().contains('location').do(
             function (indexExists) {
-                return r.branch(indexExists, {created: 0}, r.table('locations').indexCreate('location', {geo: true}));
+                return r.branch(indexExists, { created: 0 }, r.table('locations').indexCreate('location', { geo: true }));
             }
         ).run(conn);
         // Ensure begin index exists on table tracks
         await r.table('tracks').indexList().contains('begin').do(
             function (indexExists) {
-                return r.branch(indexExists, {created: 0}, r.table('tracks').indexCreate('begin'));
+                return r.branch(indexExists, { created: 0 }, r.table('tracks').indexCreate('begin'));
+            }
+        ).run(conn);
+        // Ensure tagCounter index exists on table users
+        await r.table('users').indexList().contains('tagCounter').do(
+            function (indexExists) {
+                return r.branch(indexExists, { created: 0 }, r.table('users').indexCreate('tagCounter'));
             }
         ).run(conn);
         // Propagate test data
-        var formattedLocations = LocationData.locations.map(function(location){
-           location.location = r.point(location.latitude, location.longitude);
-           return location; 
+        var formattedLocations = LocationData.locations.map(function (location) {
+            location.location = r.point(location.latitude, location.longitude);
+            return location;
         });
         await r.table('locations').insert(formattedLocations).run(conn);
     }
