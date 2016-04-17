b29439f89afe9bc9ceb7163b43319e167f8e2ee6
fdieb <hackaburg@franz-diebold.de>
2016-04-17 08:06:38 +0200

    Added second login button

--- frontend/src/BarSoundz/app/components/header/header.ts
+++ frontend/src/BarSoundz/app/components/header/header.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'header',
+    directives: [ROUTER_DIRECTIVES],
     templateUrl: 'app/components/header/header.html'
 })
 export class HeaderComponent {
